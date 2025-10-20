cc9ccfba7dc2c76adbc48db1d142065b3c3e6235
Marton <grihangne@gmail.com>
2025-10-20 19:23:43 +1100

    Add new FerryTimes.Web project

---
+++ src/FerryTimes.Web/Program.cs
@@ -0,0 +1,32 @@
+using FerryTimes.Core.Data;
+using FerryTimes.Core.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+// Add FerryTimes.Core services
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<TimetableScraperService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
